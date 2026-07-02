2faf588923b0e0d6d6e56f86f4f4230888a94d2f
ewerton martins <ewerton.thierry@gmail.com>
2026-07-02 16:40:32 -0300

    revisao

---
+++ atv5.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print("soma:", soma)
+subtração = numero1 - numero2
+print("subtração:", subtração)
+multiplicação = numero1 * numero2
+print("multiplicação:", multiplicação)
+divisão = numero1 / numero2
+print("divisão:", divisão)
